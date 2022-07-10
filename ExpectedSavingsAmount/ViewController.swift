7f1d2b2fcb143e8fa7a2b576bcb4b5ade6e23b51
tesddev <tesleem.amuda@gmail.com>
2022-07-10 22:14:06 +0100

    add date extension

--- ExpectedSavingsAmount/ViewController.swift
+++ ExpectedSavingsAmount/ViewController.swift
@@ -121,3 +121,11 @@ class ViewController: UIViewController {
     }
 }
 
+extension Date {
+   func getFormattedDate(format: String) -> String {
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = format
+        return dateformat.string(from: self)
+    }
+}
+
